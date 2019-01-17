6a8f8b67a98a8bf2d153ae4069a1e71303d02202
Anton Wyrowski <anton.wyrowski@gmail.com>
2019-01-17 13:14:43 +0100

    Updates

---
+++ precache-manifest.e37444b7fd5f193fd9f3c0ff0ffd00da.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "00b662831dce12878d558767c7925365",
+    "url": "/got-web-page/static/media/got-logo.00b66283.svg"
+  },
+  {
+    "revision": "0a8a9f5704409667040f",
+    "url": "/got-web-page/static/css/main.4d7903f8.chunk.css"
+  },
+  {
+    "revision": "59e3991a617e08f52df8",
+    "url": "/got-web-page/static/js/1.59e3991a.chunk.js"
+  },
+  {
+    "revision": "f0cc8438f3683b6155a1",
+    "url": "/got-web-page/static/js/runtime~main.f0cc8438.js"
+  },
+  {
+    "revision": "8579337a2509ebc888d021ef3b4f38d3",
+    "url": "/got-web-page/static/media/daenerys.8579337a.jpg"
+  },
+  {
+    "revision": "9ed1f5eedb8e91271c1a5d6341e190e1",
+    "url": "/got-web-page/static/media/jon-snow.9ed1f5ee.jpeg"
+  },
+  {
+    "revision": "0a8a9f5704409667040f",
+    "url": "/got-web-page/static/js/main.0a8a9f57.chunk.js"
+  },
+  {
+    "revision": "0aa4d20d0ca93d0ceec32362c8c7ec7d",
+    "url": "/got-web-page/static/media/search-icon.0aa4d20d.svg"
+  },
+  {
+    "revision": "8fe8dd266e516ce431856e2c5c600b50",
+    "url": "/got-web-page/static/media/got-dragon.8fe8dd26.jpg"
+  },
+  {
+    "revision": "16ba98ac0076ce9da72467965f0f7375",
+    "url": "/got-web-page/static/media/arya-stark.16ba98ac.jpg"
+  },
+  {
+    "revision": "deecd113e080c0d150fc62a939457a05",
+    "url": "/got-web-page/static/media/tyrion-lannister.deecd113.png"
+  },
+  {
+    "revision": "cfe4bf4c3e6a2d00ed909f9954627aca",
+    "url": "/got-web-page/index.html"
+  }
+];
