bd3c152cf87c358a71e4b40a6281b593bab40cc8
Anton Wyrowski <anton.wyrowski@gmail.com>
2019-01-20 21:56:42 +0100

    Updates

---
+++ precache-manifest.6ece24d9e5efc4b0fe3ac42fcc42e009.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "0aa4d20d0ca93d0ceec32362c8c7ec7d",
+    "url": "/got-web-page/static/media/search-icon.0aa4d20d.svg"
+  },
+  {
+    "revision": "88ba698f9f9793dd16b1",
+    "url": "/got-web-page/static/css/main.99df71d1.chunk.css"
+  },
+  {
+    "revision": "c69ccbf9ce1cf3cad4b3",
+    "url": "/got-web-page/static/js/1.c69ccbf9.chunk.js"
+  },
+  {
+    "revision": "f0cc8438f3683b6155a1",
+    "url": "/got-web-page/static/js/runtime~main.f0cc8438.js"
+  },
+  {
+    "revision": "ff1c04d7ddadf5180da67dc030f2a3bb",
+    "url": "/got-web-page/static/media/arrow.ff1c04d7.svg"
+  },
+  {
+    "revision": "8579337a2509ebc888d021ef3b4f38d3",
+    "url": "/got-web-page/static/media/daenerys.8579337a.jpg"
+  },
+  {
+    "revision": "9ed1f5eedb8e91271c1a5d6341e190e1",
+    "url": "/got-web-page/static/media/jon-snow.9ed1f5ee.jpeg"
+  },
+  {
+    "revision": "16ba98ac0076ce9da72467965f0f7375",
+    "url": "/got-web-page/static/media/arya-stark.16ba98ac.jpg"
+  },
+  {
+    "revision": "deecd113e080c0d150fc62a939457a05",
+    "url": "/got-web-page/static/media/tyrion-lannister.deecd113.png"
+  },
+  {
+    "revision": "8fe8dd266e516ce431856e2c5c600b50",
+    "url": "/got-web-page/static/media/got-dragon.8fe8dd26.jpg"
+  },
+  {
+    "revision": "a321e87275aca0d481e5043242774e1e",
+    "url": "/got-web-page/static/media/main-theme.a321e872.mp3"
+  },
+  {
+    "revision": "00b662831dce12878d558767c7925365",
+    "url": "/got-web-page/static/media/got-logo.00b66283.svg"
+  },
+  {
+    "revision": "88ba698f9f9793dd16b1",
+    "url": "/got-web-page/static/js/main.88ba698f.chunk.js"
+  },
+  {
+    "revision": "94ac2bf19193f1f4369698d5e22ab65b",
+    "url": "/got-web-page/static/media/got-wall.94ac2bf1.jpg"
+  },
+  {
+    "revision": "8e773c20c996ac7be58b89f3eff14a9f",
+    "url": "/got-web-page/static/media/kings-landing.8e773c20.jpg"
+  },
+  {
+    "revision": "6087ddcf60018dfa7c1a58f194734f0e",
+    "url": "/got-web-page/static/media/braavos.6087ddcf.jpg"
+  },
+  {
+    "revision": "241050e70a4adf87f658220770da3090",
+    "url": "/got-web-page/static/media/winterfell.241050e7.jpg"
+  },
+  {
+    "revision": "12206767067ce024a98d08b8af062c4a",
+    "url": "/got-web-page/static/media/casterly-rock.12206767.png"
+  },
+  {
+    "revision": "29e0921a5cd70c415b4e971b4c2a5b7c",
+    "url": "/got-web-page/static/media/old-valyria.29e0921a.png"
+  },
+  {
+    "revision": "52dca4cd1cd5cd618b8961ec65163e70",
+    "url": "/got-web-page/static/media/quarth.52dca4cd.jpg"
+  },
+  {
+    "revision": "6ae33cf95656d30d6f2ce0efbd27305d",
+    "url": "/got-web-page/static/media/dorne.6ae33cf9.png"
+  },
+  {
+    "revision": "634ef704588918ed0e81fc01dfa31d14",
+    "url": "/got-web-page/static/media/volantis.634ef704.jpg"
+  },
+  {
+    "revision": "d7f22e400abe6284ad001de0b39aa1d1",
+    "url": "/got-web-page/static/media/got-characters.d7f22e40.jpg"
+  },
+  {
+    "revision": "e53ddf8f9dcd945c7b9f6ca6e92e2b43",
+    "url": "/got-web-page/static/media/white-walker.e53ddf8f.jpg"
+  },
+  {
+    "revision": "49ac916d3ee438231a6708eaac88f148",
+    "url": "/got-web-page/index.html"
+  }
+];
